a663aab551f0727b4fecd40fa9e8e9d514ac4c1f
simon <simon@journeyman.cc>
2017-08-08 15:34:49 +0100

    Upversion to 1.0.0

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject org.clojars.simon_brooke/internationalisation "0.1.0-SNAPSHOT"
+(defproject org.clojars.simon_brooke/internationalisation "1.0.0"
   :description "Internationalisation library for Clojure"
   :url "https://github.com/simon-brooke/internationalisation"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.taoensso/timbre "4.10.0"]
-                 [instaparse "1.4.7"]])
+                 [instaparse "1.4.7"]]
+  :plugins [[lein-codox "0.10.3"]])
