e2e06d0244fe83b632d127494d209769c2812dac
Simon Brooke <simon@journeyman.cc>
2023-01-04 19:42:41 +0000

    Added `get-message` function, with simpler parameters.
    Also brought up to date, and fixed some stylistic errors noted by Kondo, and set the default locale to the runtime default locale.

--- project.clj
+++ project.clj
@@ -1,13 +1,21 @@
-(defproject org.clojars.simon_brooke/internationalisation "1.0.3"
+(defproject org.clojars.simon_brooke/internationalisation "1.0.3-SNAPSHOT"
+  :cloverage {:output "docs/cloverage"
+              :codecov? true
+              :emma-xml? true}
+  :codox {:metadata {:doc "**TODO**: write docs"
+                     :doc/format :markdown}
+          :output-path "docs/codox"
+          :source-uri "https://github.com/simon-brooke/internationalisation/blob/master/{filepath}#L{line}"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.taoensso/timbre "6.0.4"]
+                 [instaparse "1.4.12"]
+                 [trptr/java-wrapper "0.2.3"]]
   :description "Internationalisation library for Clojure"
-  :url "https://github.com/simon-brooke/internationalisation"
+  :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.taoensso/timbre "4.10.0"]
-                 [instaparse "1.4.7"]]
-  :plugins [[lein-codox "0.10.3"]]
+  :plugins [[lein-cloverage "1.2.2"]
+            [lein-codox "0.10.7"]]
   :profiles {:dev {:resource-paths ["resources"]}}
-  :lein-release {:deploy-via :clojars}
   :signing {:gpg-key "Simon Brooke (Stultus in monte) <simon@journeyman.cc>"}
-  )
+  :url "https://github.com/simon-brooke/internationalisation")
